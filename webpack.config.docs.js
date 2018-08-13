fb4e11dc92e8885bcc4e9b85c3129896cf5ba524
ron-dadon <rondadon1@gmail.com>
2018-08-13 16:39:10 +0300

    Remove unneeded modules

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const SIZE_100KB = 100 * 1024 * 1024
-
 module.exports = {
   entry: {
     validation: path.resolve(__dirname, 'src/docs/validation.js'),
@@ -19,21 +17,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        loader: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: SIZE_100KB
-            }
-          }
-        ]
       }
     ]
   },
